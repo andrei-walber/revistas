88fbf3b9c735647fa2e8b7ca3cc335199663f56b
Walber <andrei.walber@sap.com>
2022-05-23 18:21:32 -0300

    vai te deitar

--- wp-content/themes/revistas/fetch.php
+++ wp-content/themes/revistas/fetch.php
@@ -1,2 +1,28 @@
 <?php
 include('dbcon.php');
+
+$query = $mysql->query("SELECT * FROM rv_revistas ORDER BY id DESC");
+$total_row = mysqli_num_rows($query);
+
+$output = '';
+
+if ($total_row > 0) {
+    while ($row = $query->fetch_object()) {
+        $output .= '
+            <tr>
+                <td>' . $row->nome . '</td>
+                <td>' . $row->area_escopo . '</td>
+                <td>' . $row->localizacao . '</td>
+                <td>' . $row->qualis . '</td>
+                <td>' . $row->status . '</td>
+                <td>' . $row->tempo_medio_aceitacao . '</td>
+            </tr>';
+    }
+} else {
+    $output .= '
+        <tr>
+            <td colspan="6">Nenhuma revista encontrada!</td>
+        <tr>';
+}
+
+echo $output;
--- wp-content/themes/revistas/index.php
+++ wp-content/themes/revistas/index.php
@@ -10,6 +10,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css" rel="stylesheet">
+
     <title>Revistas Acadêmicas</title>
 </head>
 
@@ -18,15 +21,27 @@
         <br>
 
         <h2 style="text-align: center">Lista Revistas Acadêmicas</h2>
-        <div style="clear:both"></div>
 
+        <select name="multi_search_filter" id="multi_search_filter" class="form-control selectpicker" multiple>
+            <?php
+            include('dbcon.php');
+
+            $query = $mysql->query("SELECT DISTINCT area_escopo FROM rv_area_escopo ORDER BY area_escopo ASC");
+            while ($row = $query->fetch_object()) {
+                $area_escopo = $row->area_escopo;
+                echo '<option value="' . $area_escopo . '">' . $area_escopo . '</option>';
+            }
+            ?>
+        </select>
+
+        <div style="clear:both"></div>
         <br>
 
         <div class="table-responsive">
             <table class="table table-striped table-bordered">
                 <thead>
                     <th style="text-align: center">Nome</th>
-                    <th style="text-align: center">Área/escopo</th>
+                    <th style="text-align: center">Área / Escopo</th>
                     <th style="text-align: center">Localização</th>
                     <th style="text-align: center">Qualis</th>
                     <th style="text-align: center">Status</th>
@@ -42,12 +57,14 @@
         $(document).ready(function() {
             load_data()
 
-            function load_data(query='') {
+            function load_data(query = '') {
                 $.ajax({
                     url: "wp-content/themes/revistas/fetch.php",
                     method: "POST",
-                    data: {query: query},
-                    success:function(data) {
+                    data: {
+                        query: query
+                    },
+                    success: function(data) {
                         $('tbody').html(data)
                     }
                 })
